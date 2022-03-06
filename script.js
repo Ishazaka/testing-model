9bffb264921f9a2aefd9d158018c73c743be2744
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-03-06 13:33:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+
+var typed = new Typed('.auto-type', {
+  
+    strings: ["ISHA ZAKA ...","A Web Developer","A Software Engineer"],
+    typeSpeed: 40,
+    backSpeed:20,
+    loop: true
+  });
+
+
+
